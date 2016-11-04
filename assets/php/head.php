c1ec6ba7174674c5e0f6e3a851ff6e195f8d3ac0
afivos <afivos@me.com>
2016-11-04 15:51:07 +0200

    Add Graublau webfont

--- assets/php/head.php
+++ assets/php/head.php
@@ -15,21 +15,8 @@
     <title>Millouse Cereals</title>
     <meta name="description" content="">
 
-    <!-- Graublau Sans Pro font, 30-day trial -->
-    <script type="text/javascript">
-        (function() {
-            var path = '//easy.myfonts.net/v2/js?sid=156059(font-family=Graublau+Sans+Bold+Italic)&sid=156063(font-family=Graublau+Sans+ExtraLight)&sid=156067(font-family=Graublau+Sans+SemiBold)&sid=156068(font-family=Graublau+Sans+Regular)&key=tLA3eVkqpD',
-                protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
-                trial = document.createElement('script');
-            trial.type = 'text/javascript';
-            trial.async = true;
-            trial.src = protocol + path;
-            var head = document.getElementsByTagName("head")[0];
-            head.appendChild(trial);
-        })();
-    </script>
-
     <!-- Fonts, then styles -->
+    <link rel="stylesheet" href="assets/fonts/Graublau.css">
     <link rel="stylesheet" href="assets/css/main.css">
 
     <!-- jQuery first, then all JS. -->
